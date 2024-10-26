1c6b08e526dfee2732dc8ce745659a371abe2da1
STLeee <shutingleee@gmail.com>
2024-10-27 01:20:07 +0800

    feat: set sqlalchemy log format

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,11 @@
 from app.db.models import Base
 from app.core.config import settings
+from app.utils.logging import setup_logger
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
+# set sqlalchemy logger
+setup_logger("sqlalchemy")
 
 # async engine and session
 engine = create_async_engine(settings.database_url, echo=True)
