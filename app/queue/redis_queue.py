8df777b1ff76f80a94c9248980650b2a18635ace
STLeee <shutingleee@gmail.com>
2024-10-29 10:54:39 +0800

    feat: add queue metrics

--- app/queue/redis_queue.py
+++ app/queue/redis_queue.py
@@ -1,6 +1,7 @@
 from redis.asyncio import Redis
 from app.core.config import settings
 from app.utils.logging import setup_logger
+from app.utils.metrics import metrics_queue_length, metrics_queue_push_count, metrics_queue_push_fail_count, metrics_queue_pop_count, metrics_queue_pop_fail_count
 
 logger = setup_logger(__name__)
 
@@ -10,13 +11,32 @@ redis = Redis(host=settings.redis_host, decode_responses=True)
 QUEUE_NAME = "task_queue"
 
 async def enqueue_task(task_id: str):
-    await redis.rpush(QUEUE_NAME, task_id)
+    metrics_queue_push_count.inc()
+
+    try:
+        await redis.rpush(QUEUE_NAME, task_id)
+    except Exception as e:
+        metrics_queue_push_fail_count.inc()
+        e = Exception(f"Failed to push task {task_id} to redis queue: {e}")
+        logger.error(e)
+        raise e
+    
+    metrics_queue_length.inc()
     logger.info(f"Enqueued task {task_id}")
 
 async def dequeue_task():
-    task = await redis.blpop(QUEUE_NAME)
+    try:
+        task = await redis.blpop(QUEUE_NAME)
+    except Exception as e:
+        metrics_queue_pop_fail_count.inc()
+        e = Exception(f"Failed to pop task from redis queue: {e}")
+        logger.error(e)
+        raise e
+
     if task:
+        metrics_queue_pop_count.inc()
         task_id = task[1]  # Redis return (queue_name, task_id)
+        metrics_queue_length.dec()
         logger.info(f"Dequeued task {task_id}")
         return task_id
     return None
--- app/utils/metrics.py
+++ app/utils/metrics.py
@@ -1,7 +1,9 @@
 from prometheus_client import Counter, Gauge, Histogram
 
+# task status
 metrics_task_status = Gauge("task_status", "Task status", ["status"])
 
+# task operation
 metrics_task_create_request_count = Counter("task_create_request_count", "Task create request counter")
 metrics_task_create_success_count = Counter("task_create_success_count", "Task create success counter")
 metrics_task_create_fail_count = Counter("task_create_fail_count", "Task create fail counter")
@@ -10,6 +12,14 @@ metrics_task_cancel_request_count = Counter("task_cancel_request_count", "Task c
 metrics_task_cancel_success_count = Counter("task_cancel_success_count", "Task cancel success counter")
 metrics_task_cancel_fail_count = Counter("task_cancel_fail_count", "Task cancel fail counter")
 
+# task processing
 metrics_task_processing_duration = Histogram("task_processing_duration", "Task processing duration")
 metrics_task_processing_success_count = Counter("task_processing_success_count", "Task processing success counter")
 metrics_task_processing_fail_count = Counter("task_processing_fail_count", "Task processing fail counter")
+
+# queue
+metrics_queue_length = Gauge("queue_length", "Queue length")
+metrics_queue_push_count = Counter("queue_push_count", "Queue push counter")
+metrics_queue_push_fail_count = Counter("queue_push_fail_count", "Queue push fail counter")
+metrics_queue_pop_count = Counter("queue_pop_count", "Queue pop counter")
+metrics_queue_pop_fail_count = Counter("queue_pop_fail_count", "Queue pop fail counter")
